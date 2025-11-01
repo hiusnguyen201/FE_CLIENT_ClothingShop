b3382ddd2996b4382033eddbf30f52e66a37695d
minbaby01 <ongbeo111@gmail.com>
2025-11-01 16:10:38 +0700

    up

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: 4173,
-    allowedHosts: ['fe-client-clothingshop-ilvk.onrender.com'],
-  },
+    preview: {
+      host: "0.0.0.0",
+      port: 4173,
+      allowedHosts: [env.VITE_ALLOWED_HOST],
+    },
+  };
 });
